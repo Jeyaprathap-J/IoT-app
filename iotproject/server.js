bd0f8491aed30d1bb508b71a2e5967584df2f9b2
Jeyaprathap J <135618589+Jeyaprathap-J@users.noreply.github.com>
2024-05-31 23:31:23 +0530

    Updated nodejs to server

---
+++ iotproject/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const {SerialPort} = require('serialport');
+const {ReadlineParser} = require('@serialport/parser-readline');
+
+const app = express();
+const port = 3001; // Server port
+
+const serialPort = new SerialPort({
+    path:'COM3',
+    baudRate: 9600 
+});
+
+const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\r\n'}));
+
+// Endpoint to get data from the serial port
+app.get('/serial-data', (req, res) => {
+    parser.once('data', (data) => {
+      res.json({ data });
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+
+serialPort.on('open', () => {
+    console.log('Arduino UNO is Connected Successfully!');
+    console.log('Fetching Data...');
+});
+
+parser.on('data', (data) => {
+    console.log('Received data:',data);
+});
+
+serialPort.on('error', (err) => {
+    console.error('Error:', err.message);
+});
