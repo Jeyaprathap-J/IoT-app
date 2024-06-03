f9ef26d8556f52b0c66ad26a9def94f1311679aa
Jeyaprathap J <135618589+Jeyaprathap-J@users.noreply.github.com>
2024-06-03 19:11:03 +0530

    Arduino can listen the request from Web

--- iot-react-app/src/App.js
+++ iot-react-app/src/App.js
@@ -2,26 +2,61 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [serialData, setSerialData] = useState('');
+  const [inputData, setInputData] = useState('');
+  const [response, setResponse] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3001/serial-data');
-        const result = await response.json();
-        setSerialData(result.data);
-      } catch (error) {
-        console.error('Error fetching data', error);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/serial-data');
+      const result = await response.json();
+      setSerialData(result.data);
+    } catch (error) {
+      console.error('Error fetching data', error);
+    }
+  };
 
+  useEffect( () => {
     fetchData();
   }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if(inputData === "OFF"){
+        setInputData("ON");
+      }
+      else{
+        setInputData("OFF");
+      }
+      console.log("ID",inputData);
+      const res = await fetch('http://localhost:3001/send-data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: inputData }),
+      });
+      const result = await res.json();
+      setResponse(result.message);
+    } catch (error) {
+      setResponse('Error sending data');
+      console.error('Error:', error);
+    }
+
+    fetchData();
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Serial Port Data</h1>
+        <h1>Communication Between Arduino UNO and React application</h1>
         <p>{serialData}</p>
+        <form onSubmit={handleSubmit}>
+          <button 
+          type="submit"
+           >{serialData.includes("OFF")? 'Turn ON' : 'Turn OFF'}</button>
+        </form>
+        <p>{response}</p>
       </header>
     </div>
   );
--- iotproject/server.js
+++ iotproject/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');  // Import the cors middleware
+const bodyParser = require('body-parser');
 const {SerialPort} = require('serialport');
 const {ReadlineParser} = require('@serialport/parser-readline');
 
@@ -8,6 +9,7 @@ const port = 3001; // Server port
 
 // Use the cors middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 const serialPort = new SerialPort({
     path:'COM3',
@@ -15,6 +17,17 @@ const serialPort = new SerialPort({
 });
 
 const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\r\n'}));
+
+// Endpoint to receive data from React app and send it to Arduino
+app.post('/send-data', (req, res) => {
+    const { data } = req.body;
+    serialPort.write(data + '\n', (err) => {
+      if (err) {
+        return res.status(500).json({ message: 'Failed to send data' });
+      }
+      res.status(200).json({ message: 'Data sent successfully' });
+    });
+  });
 
 // Endpoint to get data from the serial port
 app.get('/serial-data', (req, res) => {
@@ -30,11 +43,10 @@ app.listen(port, () => {
 
 serialPort.on('open', () => {
     console.log('Arduino UNO is Connected Successfully!');
-    console.log('Fetching Data...');
 });
 
 parser.on('data', (data) => {
-    console.log('Received data:',data);
+    console.log(data);
 });
 
 serialPort.on('error', (err) => {
