bb731ccaa79d43c0edb31c3c6e14aa42f5cc1334
Jeyaprathap J <135618589+Jeyaprathap-J@users.noreply.github.com>
2024-10-25 14:28:50 +0530

    Working Code

--- iot-react-app/src/App.js
+++ iot-react-app/src/App.js
@@ -6,22 +6,60 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3001/serial-data');
+        const response = await fetch('http://127.0.0.1:8001/WeatherMonitor/Get_Data');
         const result = await response.json();
-        setSerialData(result.data);
+        console.log("Result",result);
+        setSerialData(result);
       } catch (error) {
         console.error('Error fetching data', error);
       }
     };
 
-    fetchData();
+    // fetchData();
+    setInterval(() => {
+      fetchData();
+ }, 1000);    
+ 
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Serial Port Data</h1>
-        <p>{serialData}</p>
+        {serialData && serialData.length > 0 ? (
+            <table className="table table-striped table-hover w-100 vw-table">
+              <thead>
+                <tr>
+                  <th className="text-center">
+                    <strong>Temperature</strong>
+                  </th>
+                  <th></th>
+                  <th className="text-center">
+                    <strong>Pressure</strong>
+                  </th>
+                  <th></th>
+                  <th className="text-center">
+                    <strong>Wind Speed</strong>
+                  </th>
+                  <th></th>
+                  <th className="text-center">
+                    <strong>Wind Direction</strong>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {serialData
+                  .map((data) => (
+                    <tr key={data.id}>
+                      <td align="center">{data.Temperature}</td><td></td>
+                      <td align="center">{data.Pressure}</td><td></td>
+                      <td align="center">{data.Wind_Speed}</td><td></td>
+                      <td align="center">{data.Wind_Direction}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          ):""}
       </header>
     </div>
   );
